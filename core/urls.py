419af5ae90d242cd119a07ce4e17a42b8ecc3c04
thakurroit <54647416+thakurroit@users.noreply.github.com>
2022-01-29 14:37:22 -0500

    First Commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import Home, Add_Student, Delete_Student, Update_Student
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+    path('add-student/', Add_Student.as_view(), name='add-student'),
+    path('delete-student/', Delete_Student.as_view(), name='delete-student'),
+    path('edit-student/<int:id>/', Update_Student.as_view(), name='edit-student'),
+]
